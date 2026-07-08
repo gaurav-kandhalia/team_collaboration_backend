62cc246cacda662843b46542bd64b9f59c126052
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2026-07-08 23:30:51 +0530

    feat :implement ApiResponse utility

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,16 @@
+
+
+class ApiResponse {
+    constructor({
+        statusCode , 
+        message,
+        data = null
+    }) {
+        this.statusCode = statusCode;
+        this.success = true;
+        this.message = message;
+        this.data = data;
+    }   
+}
+
+module.exports = ApiResponse;
