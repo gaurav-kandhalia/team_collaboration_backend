23abedb29472c5e7113e54c7e2af8bfc16535ced
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2026-07-13 23:43:22 +0530

    feat : implement index.js file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ const dotenv = require('dotenv');
 dotenv.config(
     { path: './.env' }
 );
-const pool = require('./config/db');
+const connectDatabse = require('./config/db');
 const app = require('./app');
 
 
+const startServer = async () => {
+    try {
+        await connectDatabse();
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to start the server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
 
 
